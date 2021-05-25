05e2f9cf475c904f899bbf60657a80ab9de5205c
EdsonSuarez <edsondz43@gmail.com>
2021-05-25 10:16:48 -0500

    servidores inicializados

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+// Importaciones
+const express = require("express");
+const mongoose = require("mongoose");
+
+// La aplicacion
+const app = express();
+app.use(express.json());
+
+// puerto app
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Localhost:", port));
+
+mongoose
+  .connect("mongodb://localhost:27017/boardEdson", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB: Inicializado"))
+  .catch((err) => console.log("MongoDB Error: ", err));
