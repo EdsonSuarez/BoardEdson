86698792e30fceba483d96068054a99e32722a77
EdsonSuarez <edsondz43@gmail.com>
2021-06-09 15:59:05 -0500

    Frontend add Header run

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # BoardEdson
 
-### instalacion 
+### instalacion Backend
 
 ```
 npm init
 npm install express mongoose moment jsonwebtoken jwt-simple bcrypt connect-multiparty --save
 npm install nodemon --save-dev
+
 ```
 
-## Text1
+### Installacion Frontend
+ng serve //para correr el servidor
+
+
+``
+ng generate component home/header // para crear component
+ng g c admin/update-role  // para crear component
+ng g s services/token-interceptor // para crear service
+ng g guard guard/auth
+
+
+```
+ng add @angular/material //para diseño y efectos visuales
+npm i @angular/flex-layout @angular/cdk // para que funcione bien el material
+npm audit fix --force//para que no aparezcan advertencias
+```
+
 #### Text2
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,60 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './home/login/login.component';
+import { ListTaskComponent } from './board/list-task/list-task.component';
+import { SaveTaskComponent } from './board/save-task/save-task.component';
+import { RegisterUserComponent } from './admin/register-user/register-user.component';
+import { ListUserComponent } from './admin/list-user/list-user.component';
+import { UpdateUserComponent } from './admin/update-user/update-user.component';
+import { RegisterRoleComponent } from './admin/register-role/register-role.component';
+import { ListRoleComponent } from './admin/list-role/list-role.component';
+import { UpdateRoleComponent } from './admin/update-role/update-role.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'listTasks',
+    component: ListTaskComponent,
+  },
+  {
+    path: 'saveTask',
+    component: SaveTaskComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterUserComponent,
+  },
+  {
+    path: 'updateUser',
+    component: UpdateUserComponent,
+  },
+  {
+    path: 'listUsers',
+    component: ListUserComponent,
+  },
+  
+  {
+    path: 'listRole',
+    component: ListRoleComponent,
+  },
+  {
+    path: 'registerRole',
+    component: RegisterRoleComponent,
+  },
+  {
+    path: 'updateRole',
+    component: UpdateRoleComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
